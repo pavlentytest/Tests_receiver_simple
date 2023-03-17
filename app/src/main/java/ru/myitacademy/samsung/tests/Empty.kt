839d3953c27ae36f0f1b1cd5642c0b455eede861
Pavel <impetus@yandex.ru>
2023-03-17 18:44:50 +0300

    Simplify

--- app/src/main/java/ru/myitacademy/samsung/tests/Empty.kt
+++ app/src/main/java/ru/myitacademy/samsung/tests/Empty.kt
@@ -9,16 +9,9 @@ class Empty: AppCompatActivity() {
        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
            //регистрируем первый приёмник. Сначала создаём фильтр, затем применяем
-        val if1 =  IntentFilter("ru.myitacademy.samsung.advertising.POST")
-            //изменения в упражнении 4.7.2
-           if1.priority = -1
-            registerReceiver(MyReceiver(),if1)
-           //регистрируем второй приёмник. Создаём фильтр при регистрации
-            registerReceiver(MyReceiver2(),
-                             IntentFilter("ru.myitacademy.samsung.advertising.POST")
-                                 // изменение для упражнения 4.7.2
-                                   .apply { priority = 1 }
-            )
+             val if1 =  IntentFilter("ru.myitacademy.samsung.advertising.POST")
+             registerReceiver(MyReceiver(),if1)
+
         }
     }
 
--- app/src/main/java/ru/myitacademy/samsung/tests/MyReceiver.kt
+++ app/src/main/java/ru/myitacademy/samsung/tests/MyReceiver.kt
@@ -10,15 +10,5 @@ import android.widget.Toast
 class MyReceiver: BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent) {
        Toast.makeText(p0,p1.getStringExtra("hot_news"),Toast.LENGTH_SHORT).show()
-        Log.e("REW", p1.action + p1.getStringExtra("hot_news"))
-    }
-}
-
-class MyReceiver2: BroadcastReceiver() {
-    override fun onReceive(p0: Context?, p1: Intent) {
-       val t = Toast.makeText(p0,p1.getStringExtra("hot_news"),Toast.LENGTH_SHORT)
-           t.setGravity(Gravity.TOP,0,0)
-           t.show()
-        Log.e("REW", p1.action + p1.getStringExtra("hot_news"))
     }
 }
